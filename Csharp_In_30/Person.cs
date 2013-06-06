ba6641a5582b249ccd14436430abb3c6e38b2691
musicm122 <musicm122@gmail.com>
2013-06-06 14:33:09 -0400

    init commit

---
+++ Csharp_In_30/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_In_30
+{
+    public class Person
+    {
+        public Person()
+        {
+        }
+
+        public Person(string name)
+        {
+            this.Name = name;
+        }
+
+        public string Name { get; set; }
+
+    }
+
+}
---
+++ Csharp_In_30/Program.cs
@@ -0,0 +1,29 @@
+﻿//this states the Namespaces used in the application 
+using System;
+
+namespace Csharp_In_30
+{
+    //
+    public class Program
+    {
+        static void Main()
+        {
+            //The user of this application
+            Person user;
+
+            Console.WriteLine("Ello Mate.\r\nWhat is your name?");
+            user = new Person(Console.ReadLine());
+
+            if (String.IsNullOrWhiteSpace(user.Name))
+            {
+                Console.WriteLine("Well thats just a mite bit bloody rude. Good day to you indeed");
+            }
+            else
+            {
+                Console.WriteLine("Well nice to meet you. " + user.Name);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
